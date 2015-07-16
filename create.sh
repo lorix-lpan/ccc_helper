eacf853bc2029a25199b04ef4b1e6c44efca75bb
Lawrence Pan <lawrence_pan@hot-shot.com>
2015-07-16 12:53:47 -0400

    v1.0 release :-)

--- create.sh
+++ create.sh
@@ -21,6 +21,11 @@ evaluation(){
 }
 
 extract_data(){
+  # This function only supports up to 2004
+  if (("$2" < 2004));then
+    echo "This year is not supported"
+    exit 1
+  fi
   # Get test data
   # $1 => input(y/n)
   # $2 => year
@@ -31,11 +36,26 @@ extract_data(){
     num=$RANDOM
     mkdir ~/data-"$num"
     cd ~/data-"$num"
-    if (("$2" >= 2009));then
-      wget https://cemc.math.uwaterloo.ca/contests/computing/"$2"/stage1/Stage1Data/UNIX_OR_MAC.zip
-    else
-      wget https://cemc.math.uwaterloo.ca/contests/computing/"$2"/stage1/Stage1Data/data.zip
+    # A more elegent way of finding this route is coming soon
+    # I am thinking about downloading the entire html file then use grep to find
+    # the exact route
+    local first
+    local second
+    local download_zip
+    if (("$2" >= 2009 && "$2" <= 2013));then
+      first="stage1/"
+      second="Stage1Data/"
+      download_zip="UNIX_OR_MAC.zip"
+    elif (("$2" >= 2014));then
+      first=""
+      second=""
+      download_zip="UNIX_OR_MAC.zip"
+    elif (("$2" >= 2004 && "$2" < 2009));then
+      first="stage1/"
+      second=""
+      download_zip="data.zip"
     fi
+    wget https://cemc.math.uwaterloo.ca/contests/computing/"$2"/"$first""$second""$download_zip"
     local zip_name
     zip_name=$(ls | grep .zip)
     if [[ -e "$zip_name" ]];then
@@ -43,30 +63,43 @@ extract_data(){
       rm -r "$zip_name"
       # Since there is only one file/directory in the current dir
       local folder
-      if (("$2" >= 2009));then
-        folder=$(ls | grep .*_.*_.*)
-      else
-        folder=$(ls | grep .)
-      fi
+      possible_name=("data" ".+_.+_.+")
+      for i in ${possible_name[@]}; do
+        folder=$(ls | grep -E -w "$i")
+        if [[ "$folder" != "" ]];then
+          break
+        fi
+      done
+      #if (("$2" >= 2009));then
+      #  folder=$(ls | grep .*_.*_.*)
+      #else
+      #  folder=$(ls | grep .)
+      #fi
       cd "$folder"
+      local name_file_
       if (($(ls -1 | wc -l) <= 3));then
+        # ls -1 | wc -l returns the number of files/dirs in current dir
+        # if the files are contained seperately in junior and senior parent dir
         # cd into senior/junior
         # Bash 4.0 sytax, downcase all the letters of the string $level
         # "${3,,}"
         cd "${3,,}"
-        local name_file_
         name_file_=$(ls | grep "$4")
         cp "$name_file_"/* "$5"
         echo "Success"
         clean_garbage
       else
-        echo "The operation is not currently supported"
+        # If all the subdirs are placed in one parent dir(number of dirs > 3)
+        # Substring, get the first letter of $level
+        # "${3:0:1}"
+        # grep -i ignores case
+        name_file_=$(ls | grep -i "${3:0:1}$4")
+        cp "$name_file_"/* "$5"
+        echo "Success"
         clean_garbage
         exit 1
       fi
 
-      # Substring, get the first letter of $level
-      # "${3:0:1}"
       #if [[ -d "$5" ]];then
       #  cp "${3,,}"/"${3:0:1}"/* "$5"
       #  echo "success"
